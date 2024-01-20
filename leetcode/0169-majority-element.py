de292d1e1204705675f435d1892f389f961b1063
realrains <real.longrain@gmail.com>
2024-01-20 17:55:00 +0900

    169

---
+++ leetcode/0169-majority-element.py
@@ -0,0 +1,29 @@
+from typing import List
+
+class Solution:
+    # Boyer-Moore Majority Vote Algorithm
+    def majorityElement(self, nums: List[int]) -> int:
+        count = 0
+        major = None
+
+        for num in nums:
+            if count == 0:
+                major = num
+            elif major == num:
+                count += 1
+            else:
+                count -= 1
+        
+        return major
+
+    # def majorityElement(self, nums: List[int]) -> int:
+    #     counter = dict()
+    #     for num in nums:
+    #         if num in counter:
+    #             counter[num] += 1
+    #         else:
+    #             counter[num] = 1
+        
+    #     for num, count in counter.items():
+    #         if count > len(nums) // 2:
+    #             return num
