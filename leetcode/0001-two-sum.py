6e734cee9700e86139c3e9e086dd4a59b2de438d
realrains <real.longrain@gmail.com>
2024-05-13 01:15:38 +0900

    0001

--- leetcode/0001-two-sum.py
+++ leetcode/0001-two-sum.py
@@ -1,9 +1,16 @@
-from typing import List
-
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        N = len(nums)
-        for i in range(N):
-            for j in range(i + 1, N):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        idx_nums = list(enumerate(nums))
+        idx_nums.sort(key=lambda e: e[1])
+        i, j = 0, len(nums)-1
+
+        while i < j:
+            s = idx_nums[i][1] + idx_nums[j][1]
+
+            if s == target:
+                return [idx_nums[i][0], idx_nums[j][0]]
+
+            if s < target:
+                i += 1
+            else:
+                j -= 1
