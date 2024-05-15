c5d86ef2846b509f428a7641f0acc0492e602762
realrains <real.longrain@gmail.com>
2024-05-15 15:55:07 +0900

    0101 revised

--- leetcode/0101-symmtric-tree.py
+++ leetcode/0101-symmtric-tree.py
@@ -4,40 +4,15 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from typing import Optional
-
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        left = []
-        right = []
 
-        def preorder(node):
-            if not node:
-                left.append(None)
-                return
-            left.append(node.val)
-            preorder(node.left)
-            preorder(node.right)
-        
-        def lastorder(node):
-            if not node:
-                right.append(None)
-                return
-            right.append(node.val)
-            lastorder(node.right)
-            lastorder(node.left)
-        
-        preorder(root.left)
-        lastorder(root.right)
-
-        return left == right
-
-        
-
+        def compare(left, right):
+            if not left or not right:
+                return left == right
+            if left.val != right.val:
+                return False
             
+            return compare(left.left, right.right) and compare(left.right, right.left)
+
+        return compare(root.left, root.right)
