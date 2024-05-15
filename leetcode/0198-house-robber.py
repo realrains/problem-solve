60682e0dfe5516e9cd8893b4cba7abe687bc4c3d
realrains <real.longrain@gmail.com>
2024-05-15 22:50:20 +0900

    0198 revised

--- leetcode/0198-house-robber.py
+++ leetcode/0198-house-robber.py
@@ -1,20 +1,17 @@
-from typing import List
-
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [-1 for _ in range(len(nums))]
+        n = len(nums)
 
-        def recursive(n):
-            if n >= len(nums):
-                return 0
-            if dp[n] > 0:
-                return dp[n]
-            dp[n] = nums[n] + max(recursive(n+2), recursive(n+3))
-            return dp[n]
+        if n == 1:
+            return nums[-1]
+        if n == 1:
+            return max(nums[-1], nums[-2])
 
-        for i in range(len(nums)):
-            recursive(i)
-
-        return max(dp)
-
+        dp = [0] * n
+        dp[-1] = nums[-1]
+        dp[-2] = max(nums[-1], nums[-2])
         
+        for i in reversed(range(len(nums)-2)):
+            dp[i] = max(nums[i] + dp[i+2], dp[i+1])
+        
+        return dp[0]
