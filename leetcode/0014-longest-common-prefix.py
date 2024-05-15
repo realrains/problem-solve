a319d1573a5baec4584c590798e293efac467edd
realrains <real.longrain@gmail.com>
2024-05-15 15:14:35 +0900

    0014 revised

--- leetcode/0014-longest-common-prefix.py
+++ leetcode/0014-longest-common-prefix.py
@@ -1,11 +1,7 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         strs.sort()
-        prefix = ""
-        for c1, c2 in zip(strs[0], strs[-1]):
-            if c1 != c2:
-                return prefix
-            else:
-                prefix += c1
-        
-        return prefix
+        for i, cc in enumerate(zip(strs[0], strs[-1])):
+            if cc[0] != cc[1]:
+                return strs[0][:i]
+        return strs[0]
