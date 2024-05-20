a622f9cfae651b490f8d8bb82ffcfc37395b8ee6
realrains <real.longrain@gmail.com>
2024-05-20 23:07:55 +0900

    0049

---
+++ leetcode/0049-group-anagrams.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        group = {}
+
+        for word in strs:
+            key = ''.join(sorted(word))
+            if key in group:
+                group[key].append(word)
+            else:
+                group[key] = [word]
+        
+        return group.values()
